518e2ba3e511e55e18124acd607889abc42c49d4
Nikhilkowluru7 <nikhilkowluru@gmail.com>
2026-01-17 21:11:30 +0000

    Bus fare HW.py

--- School bus.py
+++ School bus.py
@@ -1,11 +1,17 @@
 class vehicle:
-    def __init__(self,name,max_speed,mileage):
+    def __init__(self,name,max_speed,mileage,capacity):
         self.name = name
         self.max_speed = max_speed
         self.mileage = mileage
+        self.capacity = capacity
+    def fare(self):
+        return self.capacity*100 
 
 class bus(vehicle):
-    pass
+    def fare(self):
+        maintainence = self.capacity*100
+        return maintainence+maintainence*0.1
+    
 
-obj = bus("Mercedes Bus",150,25)
-print("Name: ",obj.name,"Max speed: ",obj.max_speed,"Mileage: ",obj.mileage)
+obj = bus("Mercedes Bus",150,25,50)
+print("Name: ",obj.name,"Max speed: ",obj.max_speed,"Mileage: ",obj.mileage,"Fare: ",obj.fare())
